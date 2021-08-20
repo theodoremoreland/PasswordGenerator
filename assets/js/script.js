9d3673dc8d3c49b3c3b3f0b25f9668250026471d
Theodore Moreland <theodoremoreland@yahoo.com>
2021-08-20 01:10:24 -0500

    version without classes and helpers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,101 @@
-import Criterion from "./classes/Criterion.js";
-import { criterionPrompt } from "./criterionPrompt.js";
-
-const desiredPasswordLength = new Criterion("Length of password (must be between 8-128)?", "128", "range");
-const shouldHaveLowercaseLetters = new Criterion("Include lowercase letters (yes/no)?", "yes", "binary");
-const shouldHaveUppercaseLetters = new Criterion("Include UPPERCASE letters (yes/no)?", "yes", "binary");
-const shouldHaveNumbers = new Criterion("Include numbers (yes/no)?", "yes", "binary");
-const shouldHaveSpecialCharacters = new Criterion("Include special characters (yes/no)?", "yes", "binary");
 const generateBtn = document.querySelector("#generate");
 
-const getUserSelectedCriteria = () => {
-    const length = criterionPrompt(desiredPasswordLength);
-    const useLowercaseLetters = criterionPrompt(shouldHaveLowercaseLetters);
-    const useUppercaseLetters = criterionPrompt(shouldHaveUppercaseLetters);
-    const useNumbers = criterionPrompt(shouldHaveNumbers);
-    const useSpecialCharacters = criterionPrompt(shouldHaveSpecialCharacters);
+const promptUserForDesiredPasswordLength = (minLength = 8, maxLength = 128, defaultValue = 16) => {
+    const responseIsValid = response => response >= minLength && response <= maxLength;
+    const promptText = `Length of password (must be between ${minLength}-${maxLength})?\nDefault value = ${defaultValue}`;
+    const errorText = `Password length must be a number between ${minLength}-${maxLength}`;
+    const desiredPasswordLength = prompt(promptText, defaultValue).trim();
 
-    return {
-        length,
-        useLowercaseLetters,
-        useUppercaseLetters,
-        useNumbers,
-        useSpecialCharacters
+    if (responseIsValid(desiredPasswordLength)) {
+        return +desiredPasswordLength;
     }
+
+    alert(errorText);
+
+    return promptUserForDesiredPasswordLength(minLength, maxLength, defaultValue);
+};
+
+
+const promptUserToApproveChracterSet = (characterSetMetadata) => {
+    const responseIsValid = (response) => {
+        const regex = /^(yes|y|1|no|n|0)$/i;
+        return regex.test(response);
+    };
+
+    const defaultValue = characterSetMetadata.approved === true ? "yes" : "no";
+    const promptText = `Include ${characterSetMetadata.name} (yes/no)?\nDefault value = ${defaultValue}`;
+    const errorText = `Invalid response. Response must be "yes" or "no"`;
+    const response = prompt(promptText, defaultValue).trim();
+    
+    if (!responseIsValid) {
+        alert(errorText);
+
+        return promptUserToApproveChracterSet(characterSetMetadata);
+    }
+
+    const userApproved = ["yes", "y", "1"].includes(response.toLowerCase());
+
+    if (userApproved) {
+        characterSetMetadata.approved = true;
+    } else {
+        characterSetMetadata.approved = false;
+    }
+};
+
+const promptUserForCriteria = () => {
+    const characterSets = {
+        lowercaseLetters: {
+            name: "lowercase letters",
+            set: "abcdefghijklmnopqrstuvwxyz",
+            approved: true
+        },
+        uppercaseLetters: {
+            name: "uppercase letters",
+            set: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            approved: true
+        },
+        numbers: {
+            name: "numbers",
+            set: "0123456789",
+            approved: true
+        },
+        specialCharacters: {
+            name: "special characters",
+            set: [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"],
+            approved: true
+        }
+    };
+    const desiredPasswordLength = promptUserForDesiredPasswordLength();
+
+    for (const characterSetMetadata of Object.values(characterSets)) {
+        promptUserToApproveChracterSet(characterSetMetadata);
+    }
+
+    return { desiredPasswordLength, characterSets };
 }
 
-const generatePassword = (criteria) => {
-    const { 
-        length,
-        useLowercaseLetters,
-        useUppercaseLetters,
-        useNumbers,
-        useSpecialCharacters
-    } = criteria; 
-    const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
-    const uppercaseLetters = lowercaseLetters.toUpperCase();
-    const numbers = "0123456789";
-    const specialCharacters = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+const generatePassword = ({ desiredPasswordLength, characterSets }) => {
     let generatedPassword = "";
-    let characterSets = {};
+    const approvedCharacterSets = Object.values(characterSets).map(metadata => metadata.approved ? metadata.set : undefined).filter(e => e);
 
-    if (["yes", "1", "y"].includes(useLowercaseLetters)) characterSets[lowercaseLetters] = lowercaseLetters;
-    if (["yes", "1", "y"].includes(useUppercaseLetters)) characterSets[uppercaseLetters] = uppercaseLetters;
-    if (["yes", "1", "y"].includes(useNumbers)) characterSets[numbers] = numbers;
-    if (["yes", "1", "y"].includes(useSpecialCharacters)) characterSets[specialCharacters] = specialCharacters;
-    
-    const keySetCount = Object.keys(characterSets).length;
-    let currentKeyIndex = 0;
+    if (approvedCharacterSets.length === 0) {
+        alert("Failed to generate password because no character set was chosen.");
+        return generatedPassword;
+    }
 
-    if (keySetCount === 0) alert("Could not generate password because no character set was chosen.");
-
-    for (let i = 0; i < length; i++) {
-        const currentKey = Object.keys(characterSets)[currentKeyIndex];
-        const currentSet = characterSets[currentKey];
-        generatedPassword += currentSet[Math.floor(Math.random() * currentSet.length)];
-        currentKeyIndex = currentKeyIndex === (keySetCount - 1) ? 0 : currentKeyIndex + 1;
+    while (generatedPassword.length < desiredPasswordLength) {
+        for (const approvedCharacterSet of approvedCharacterSets) {
+            const randomIndex = Math.floor(Math.random() * approvedCharacterSet.length);
+            generatedPassword += approvedCharacterSet[randomIndex];
+            if (generatedPassword.length === desiredPasswordLength) break;
+        }
     }
 
     return generatedPassword;
 };
 
 const writePassword = () => {
-  const criteria = getUserSelectedCriteria();
+  const criteria = promptUserForCriteria();
   const password = generatePassword(criteria);
   const passwordText = document.querySelector("#password");
 
