d9e4e9052a7b315dbc9b17420dff881657742298
Theodore Moreland <theodoremoreland@yahoo.com>
2021-08-22 17:46:29 -0500

    added doc comment to change jest environment from node to jsdom + added one working integration test

--- assets/js/tests/Criteria.integration.test.js
+++ assets/js/tests/Criteria.integration.test.js
@@ -1,28 +1,31 @@
+/**
+ * @jest-environment jsdom
+ */
 import CharacterSet from "../classes/CharacterSet.js";
 import Criteria from "../classes/Criteria";
 
 describe("Criteria class", () => {
-    describe("Criteria class constructor", () => {
-        let lowercaseLetters;
-        let uppercaseLetters;
-        let criteria;
-    
-        beforeAll(() => {
-            lowercaseLetters = new CharacterSet(
-                "lowercase letters",
-                "abcdefghijklmnopqrstuvwxyz",
-                true
-            );
-    
-            uppercaseLetters = new CharacterSet(
-                "uppercase letters",
-                lowercaseLetters.characters.toUpperCase(),
-                false
-            );
-    
-            criteria = new Criteria(16, [lowercaseLetters, uppercaseLetters]);
-        });
-    
+    let lowercaseLetters;
+    let uppercaseLetters;
+    let criteria;
+
+    beforeAll(() => {
+        lowercaseLetters = new CharacterSet(
+            "lowercase letters",
+            "abcdefghijklmnopqrstuvwxyz",
+            true
+        );
+
+        uppercaseLetters = new CharacterSet(
+            "uppercase letters",
+            lowercaseLetters.characters.toUpperCase(),
+            false
+        );
+
+        criteria = new Criteria(16, [lowercaseLetters, uppercaseLetters]);
+    });
+
+    describe("Criteria class constructor", () => {    
         test("should set length from constructor", () => {
             expect(criteria.length).toEqual(16);
         });
@@ -35,6 +38,24 @@ describe("Criteria class", () => {
     });
 
     describe("Criteria class prompts", () => {
+        test("should set length after valid response from user", () => {
+            const promptSpy = jest.spyOn(window, 'prompt').mockReturnValueOnce("19");
 
+            criteria.promptUserForPasswordLength();
+
+            expect(promptSpy).toHaveBeenCalledTimes(1);
+            expect(criteria.length).toEqual(19);
+        });
+
+        test("should show alert after invalid response from user for password length", () => {
+            const promptSpy = jest.spyOn(window, 'prompt').mockReturnValueOnce("Hi!");
+            const alertSpy = jest.spyOn(window, "alert").mockImplementation(() => {});
+
+            criteria.promptUserForPasswordLength();
+
+            expect(promptSpy).toHaveBeenCalledTimes(1);
+            expect(alertSpy).toHaveBeenCalledTimes(1);
+            expect(criteria.length).toEqual(19);
+        });
     });
 });
