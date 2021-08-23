c101d3f72ac1edea24c0df98b2947aac5053c51e
Theodore Moreland <theodoremoreland@yahoo.com>
2021-08-23 09:11:16 -0500

    completed integration testing suite for generatePassword function: app is currently failing tests, need to fix logic error

--- assets/js/tests/generatePassword.integration.test.js
+++ assets/js/tests/generatePassword.integration.test.js
@@ -10,6 +10,7 @@ describe("generatePassword function", () => {
     let uppercaseLetters;
     let numbers;
     let specialCharacters;
+    let criteria;
     let promptSpy;
     let alertSpy;
 
@@ -50,41 +51,203 @@ describe("generatePassword function", () => {
         alertSpy.mockRestore();
     });
 
-    test("should generate password with all lowercase letters", () => {
+    test("should generate password of desired length", () => {
         promptSpy
-        .mockReturnValueOnce("YES")
-        .mockReturnValueOnce("0")
-        .mockReturnValueOnce("0")
-        .mockReturnValueOnce("0");
+            .mockReturnValueOnce("yes")
+            .mockReturnValueOnce("no")
+            .mockReturnValueOnce("no")
+            .mockReturnValueOnce("no");
 
         criteria.promptUserToApproveEachCharacterSet();
         const approvedCharacterSets = criteria.extractApprovedChracterSets();
         const approvedCharacterSetsKeys = Object.keys(approvedCharacterSets);
 
         const password = generatePassword(16, "", approvedCharacterSets, approvedCharacterSetsKeys);
+        expect(password.length).toEqual(16);
     });
 
-    test("should generate password with all uppercase letters", () => {
+    test("should generate password with only lowercase letters", () => {
+        promptSpy
+            .mockReturnValueOnce("yES")
+            .mockReturnValueOnce("no")
+            .mockReturnValueOnce("no")
+            .mockReturnValueOnce("no");
 
-        const password = generatePassword("", {}, []);
-        
+        criteria.promptUserToApproveEachCharacterSet();
+        const approvedCharacterSets = criteria.extractApprovedChracterSets();
+        const approvedCharacterSetsKeys = Object.keys(approvedCharacterSets);
+
+        const password = generatePassword(16, "", approvedCharacterSets, approvedCharacterSetsKeys);
+
+        for (const character of password) {
+            const isLowercaseLetter = lowercaseLetters.characters.includes(character);
+            
+            expect(isLowercaseLetter).toBeTruthy();
+        }
     });
 
-    test("should generate password with all numbers", () => {
+    test("should generate password with only uppercase letters", () => {
+        promptSpy
+            .mockReturnValueOnce("no")
+            .mockReturnValueOnce("yes")
+            .mockReturnValueOnce("no")
+            .mockReturnValueOnce("no");
 
-        const password = generatePassword("", {}, []);
-        
+        criteria.promptUserToApproveEachCharacterSet();
+        const approvedCharacterSets = criteria.extractApprovedChracterSets();
+        const approvedCharacterSetsKeys = Object.keys(approvedCharacterSets);
+
+        const password = generatePassword(16, "", approvedCharacterSets, approvedCharacterSetsKeys);
+
+        for (const character of password) {
+            const isUppercaseLetter = uppercaseLetters.characters.includes(character);
+            
+            expect(isUppercaseLetter).toBeTruthy();
+        }
     });
 
-    test("should generate password with all special characters", () => {
+    test("should generate password with only numbers", () => {
+        promptSpy
+            .mockReturnValueOnce("no")
+            .mockReturnValueOnce("no")
+            .mockReturnValueOnce("yes")
+            .mockReturnValueOnce("no");
 
-        const password = generatePassword("", {}, []);
-        
+        criteria.promptUserToApproveEachCharacterSet();
+        const approvedCharacterSets = criteria.extractApprovedChracterSets();
+        const approvedCharacterSetsKeys = Object.keys(approvedCharacterSets);
+
+        const password = generatePassword(16, "", approvedCharacterSets, approvedCharacterSetsKeys);
+
+        for (const character of password) {
+            const isNumber = numbers.characters.includes(character);
+            
+            expect(isNumber).toBeTruthy();
+        }  
     });
 
-    test("password has 4 character sets", () => {
+    test("should generate password with only special characters", () => {
+        promptSpy
+            .mockReturnValueOnce("no")
+            .mockReturnValueOnce("no")
+            .mockReturnValueOnce("no")
+            .mockReturnValueOnce("yes");
 
-        const password = generatePassword("", {}, []);
+        criteria.promptUserToApproveEachCharacterSet();
+        const approvedCharacterSets = criteria.extractApprovedChracterSets();
+        const approvedCharacterSetsKeys = Object.keys(approvedCharacterSets);
+
+        const password = generatePassword(16, "", approvedCharacterSets, approvedCharacterSetsKeys);
+
+        for (const character of password) {
+            const isSpecialCharacter = specialCharacters.characters.includes(character);
+            
+            expect(isSpecialCharacter).toBeTruthy();
+        } 
+    });
+
+    test("should generate password featuring 2 character sets", () => {
+        promptSpy
+            .mockReturnValueOnce("yes")
+            .mockReturnValueOnce("yes")
+            .mockReturnValueOnce("no")
+            .mockReturnValueOnce("no");
+
+        criteria.promptUserToApproveEachCharacterSet();
+        const approvedCharacterSets = criteria.extractApprovedChracterSets();
+        const approvedCharacterSetsKeys = Object.keys(approvedCharacterSets);
+
+        const password = generatePassword(16, "", approvedCharacterSets, approvedCharacterSetsKeys);
+        const charactersSetsInPassword = new Set();
+        const expected = new Set([
+            lowercaseLetters.name,
+            uppercaseLetters.name
+        ]);
+
+        for (const character of password) {
+            const isLowercaseLetter = lowercaseLetters.characters.includes(character);
+            const isUppercaseLetter = uppercaseLetters.characters.includes(character);
+            const isNumber = numbers.characters.includes(character);
+            const isSpecialCharacter = specialCharacters.characters.includes(character);
+            
+            if (isLowercaseLetter) charactersSetsInPassword.add(lowercaseLetters.name);
+            else if (isUppercaseLetter) charactersSetsInPassword.add(uppercaseLetters.name);
+            else if (isNumber) charactersSetsInPassword.add(numbers.name);
+            else if (isSpecialCharacter) charactersSetsInPassword.add(specialCharacters.name);
+            else charactersSetsInPassword.add("unknown");
+        }
         
+        expect(charactersSetsInPassword).toEqual(expected);
+    });
+
+    test("should generate password featuring 3 character sets", () => {
+        promptSpy
+            .mockReturnValueOnce("yes")
+            .mockReturnValueOnce("yes")
+            .mockReturnValueOnce("yes")
+            .mockReturnValueOnce("no");
+
+        criteria.promptUserToApproveEachCharacterSet();
+        const approvedCharacterSets = criteria.extractApprovedChracterSets();
+        const approvedCharacterSetsKeys = Object.keys(approvedCharacterSets);
+
+        const password = generatePassword(16, "", approvedCharacterSets, approvedCharacterSetsKeys);
+        const charactersSetsInPassword = new Set();
+        const expected = new Set([
+            lowercaseLetters.name,
+            uppercaseLetters.name,
+            numbers.name
+        ]);
+
+        for (const character of password) {
+            const isLowercaseLetter = lowercaseLetters.characters.includes(character);
+            const isUppercaseLetter = uppercaseLetters.characters.includes(character);
+            const isNumber = numbers.characters.includes(character);
+            const isSpecialCharacter = specialCharacters.characters.includes(character);
+            
+            if (isLowercaseLetter) charactersSetsInPassword.add(lowercaseLetters.name);
+            else if (isUppercaseLetter) charactersSetsInPassword.add(uppercaseLetters.name);
+            else if (isNumber) charactersSetsInPassword.add(numbers.name);
+            else if (isSpecialCharacter) charactersSetsInPassword.add(specialCharacters.name);
+            else charactersSetsInPassword.add("unknown");
+        }
+        
+        expect(charactersSetsInPassword).toEqual(expected);
+    });
+
+    test("should generate password featuring 4 character sets", () => {
+        promptSpy
+            .mockReturnValueOnce("yes")
+            .mockReturnValueOnce("yes")
+            .mockReturnValueOnce("yes")
+            .mockReturnValueOnce("yes");
+
+        criteria.promptUserToApproveEachCharacterSet();
+        const approvedCharacterSets = criteria.extractApprovedChracterSets();
+        const approvedCharacterSetsKeys = Object.keys(approvedCharacterSets);
+
+        const password = generatePassword(16, "", approvedCharacterSets, approvedCharacterSetsKeys);
+        const charactersSetsInPassword = new Set();
+        const expected = new Set([
+            lowercaseLetters.name,
+            uppercaseLetters.name,
+            numbers.name,
+            specialCharacters.name
+        ]);
+
+        for (const character of password) {
+            const isLowercaseLetter = lowercaseLetters.characters.includes(character);
+            const isUppercaseLetter = uppercaseLetters.characters.includes(character);
+            const isNumber = numbers.characters.includes(character);
+            const isSpecialCharacter = specialCharacters.characters.includes(character);
+            
+            if (isLowercaseLetter) charactersSetsInPassword.add(lowercaseLetters.name);
+            else if (isUppercaseLetter) charactersSetsInPassword.add(uppercaseLetters.name);
+            else if (isNumber) charactersSetsInPassword.add(numbers.name);
+            else if (isSpecialCharacter) charactersSetsInPassword.add(specialCharacters.name);
+            else charactersSetsInPassword.add("unknown");
+        }
+        
+        expect(charactersSetsInPassword).toEqual(expected);
     });
 });
